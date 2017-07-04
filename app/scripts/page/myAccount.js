ffdf9ba4fb866fffb25922c9f54ffb7638c55fc5
storm1er <le.storm1er@gmail.com>
2017-07-04 14:08:34 +0200

    préparation de la page 'mon compte'

--- app/scripts/page/myAccount.js
+++ app/scripts/page/myAccount.js
@@ -26,7 +26,17 @@
 						'<div class="col s12">'+
 							'<h2 class="center-align">Mon compte</h2>'+
 							'<p class="center-align">'+
-								'Page en cours de développement'+
+								'<div class="preloader-wrapper big active">'+
+									'<div class="spinner-layer spinner-blue-only">'+
+										'<div class="circle-clipper left">'+
+											'<div class="circle"></div>'+
+										'</div><div class="gap-patch">'+
+											'<div class="circle"></div>'+
+										'</div><div class="circle-clipper right">'+
+											'<div class="circle"></div>'+
+										'</div>'+
+									'</div>'+
+								'</div>'+
 							'</p>'+
 						'</div>'+
 					'</div>'
@@ -34,6 +44,11 @@
 				block.data('id', id);
 				block.on('update', function(event, settings) {
 					// @TODO
+					window.get().currentUser(function(response){
+						var preloader = block.find('.preloader-wrapper');
+						$('<p>currentUser = '+JSON.stringify(response)+'</p>').insertAfter(preloader);
+						preloader.remove();
+					});
 				});
 				return block;
 			}
